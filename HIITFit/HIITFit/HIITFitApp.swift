09df2641b97e5d05234d17835dc0d2710d085a1c
Arunkumar <arunkumar@Mac.lan>
2025-05-30 09:49:58 +0530

    Feat: Initial setup for app

---
+++ HIITFit/HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Arunkumar on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
