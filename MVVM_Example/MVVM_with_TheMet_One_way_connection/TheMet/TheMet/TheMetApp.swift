e1fc30a7bcfdaa14efc6d2e452b76373eda94fe8
Arunkumar <arunkumar@Mac.lan>
2025-06-27 08:57:31 +0530

    add sample project for mvvm design pattern with dependency injection

---
+++ MVVM_Example/MVVM_with_TheMet_One_way_connection/TheMet/TheMet/TheMetApp.swift
@@ -0,0 +1,49 @@
+/// Copyright (c) 2024 Kodeco LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+
+@main
+struct TheMetApp: App {
+  
+  //So TheMetApp owns the store object, now it is the source of truth and if some other view want to access store, we can simply do a pass.
+  @State private var store = TheMetStore()
+  
+  var body: some Scene {
+    WindowGroup {
+      //If 2 or more views accessing this data, passing through hierarchy makes more boiler plater. Instead you can pass it as environment object(In observation(whoever wants to access) they called it as environment)
+      //ContentView(store: store)
+      ContentView()
+        .environment(store)
+    }
+  }
+}
---
+++ WishList_SwiftData/WishList/WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by Arunkumar on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
